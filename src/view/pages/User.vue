fd3f176a1885699ff104f8eb0e7648d545907f0a
ricardosalamank <rasalamancam@gmail.com>
2020-08-06 18:01:15 -0500

    primer commit platilla meteor adelanto lookAndFeel crud usuario

---
+++ src/view/pages/User.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <!-- NAVIGATION -->
+    <nav class="navbar navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">VuejsFirebase</a>
+    </nav>
+    <!-- MAIN CONTENT -->
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-header">
+              <h3>Add a New Website</h3>
+            </div>
+            <div class="card-body">
+              <form @submit.prevent="addWebsite">
+                <div class="form-group">
+                  <input type="text" class="form-control"  placeholder="Name">
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control"  placeholder="Author">
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control"  placeholder="URL">
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  Save
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-8 text-center">
+                    <!--<img src="./assets/logo.png" alt="">-->
+          <div class="card">
+            <div class="card-header">
+              <h3>Websites List</h3>
+            </div>
+            <div class="card-body">
+              <table class="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Author</th>
+                    <th>Operations</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr >
+                    
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>

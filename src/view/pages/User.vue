208380a3e4986232fa7582a6ceb02e95e48f5adc
ricardosalamank <rasalamancam@gmail.com>
2020-08-07 16:19:19 -0500

    se termina crud vuejs en la plantilla Metronic demo3 , se agrega consumo servicio de paises del mundo https://restcountries.eu/rest/v2/all

--- src/view/pages/User.vue
+++ src/view/pages/User.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <!-- NAVIGATION -->
     <nav class="navbar navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">VuejsFirebase</a>
+      <a class="navbar-brand" href="#">Test Vuejs</a>
     </nav>
     <!-- MAIN CONTENT -->
     <div class="container">
@@ -10,7 +10,13 @@
         <div class="col-sm-4">
           <div class="card">
             <div class="card-header">
-              <h3>Add a New User</h3>
+              <h3>{{ title }}</h3>
+              <button
+                @click="cancelEdit()"
+                class="btn btn-danger"
+                v-if="newUser.id != 'new'"
+                style="float: right;"
+              >Cancel</button>
             </div>
             <div class="card-body">
               <form @submit.prevent="addUser">
@@ -18,24 +24,38 @@
                   <input type="text" class="form-control" v-model="newUser.name" placeholder="Name" />
                 </div>
                 <div class="form-group">
-                  <input type="text" class="form-control" v-model="newUser.last" placeholder="Last Name" />
+                  <input
+                    type="text"
+                    class="form-control"
+                    v-model="newUser.last"
+                    placeholder="Last Name"
+                  />
                 </div>
                 <div class="form-group">
-                  <input type="email" class="form-control" v-model="newUser.email" placeholder="Email" />
+                  <input
+                    type="email"
+                    class="form-control"
+                    v-model="newUser.email"
+                    placeholder="Email"
+                  />
                 </div>
                 <div class="form-group">
-                  <input type="text" class="form-control" v-model="newUser.country" placeholder="Country" />
+                  <select class="form-control" v-model="newUser.country">
+                    <option value disabled selected>select an option</option>
+                    <option
+                      v-for="(element, index) in countrys"
+                      :key="index"
+                      v-bind:value="element"
+                    >{{ element }}</option>
+                  </select>
                 </div>
                 <button type="submit" class="btn btn-primary">Save</button>
               </form>
             </div>
-            <div class="alert alert-danger" role="alert" v-if="error">
-              {{ error }}
-            </div>
+            <div class="alert alert-danger" role="alert" v-if="error">{{ error }}</div>
           </div>
         </div>
         <div class="col-sm-8 text-center">
-          <!--<img src="./assets/logo.png" alt="">-->
           <div class="card">
             <div class="card-header">
               <h3>User List</h3>
@@ -53,22 +73,16 @@
                 </thead>
                 <tbody>
                   <tr v-for="(user, index) in users" :key="index">
+                    <td>{{user.name}}</td>
+                    <td>{{user.last}}</td>
+                    <td>{{user.email}}</td>
+                    <td>{{user.country}}</td>
                     <td>
-                      {{user.nombres}}
-                    </td>
-                    <td>
-                      {{user.apellidos}}
-                    </td>
-                    <td>
-                      {{user.email}}
-                    </td>
-                    <td>
-                      {{user.pais}}
-                    </td>
-                    <td>
-                      <button @click="deleteUser(index)" class="btn btn-danger">
-                        Delete
-                      </button>
+                      <button @click="editUser(user)" class="btn btn-success">Edit</button> &nbsp;&nbsp;
+                      <button
+                        @click="deleteUser(user.id)"
+                        class="btn btn-danger"
+                      >Delete</button>
                     </td>
                   </tr>
                 </tbody>
@@ -83,7 +97,7 @@
 
 <script>
 import db from "@/config/fire";
-//import firebase from "firebase";
+import request from "request-promise-native";
 
 export default {
   name: "user",
@@ -91,12 +105,15 @@ export default {
     return {
       users: [],
       newUser: {
-        name: '',
-        last: '',
-        email: '',
-        country: ''
+        id: "new",
+        name: "",
+        last: "",
+        email: "",
+        country: "",
       },
-      error:''
+      error: "",
+      countrys: "",
+      title: "Add a New User",
     };
   },
   mounted() {
@@ -108,80 +125,83 @@ export default {
         this.users.push(data);
       });
     });
+    this.appendInitial();
   },
   methods: {
+    async appendInitial() {
+      let options = {
+        uri: `https://restcountries.eu/rest/v2/all`,
+        method: "GET",
+        json: true,
+      };
+      try {
+        let result;
+        result = await request(options);
+        this.countrys = result.map((x) => x.name);
+      } catch (e) {
+        console.error("error-->", e);
+        return;
+      }
+    },
     addUser: function () {
       for (var key in this.newUser) {
-        if (this.newUser[key] == null || this.newUser[key] == ""){
-          this.error = " Plz write the in field "+key;
+        if (this.newUser[key] == null || this.newUser[key] == "") {
+          this.error = " Plz write the in field " + key;
           return false;
-        }  
-    }
+        }
+      }
 
-    this.error = '';
+      if (this.newUser.id == "new") {
+        db.collection("user")
+          .add(this.newUser)
+          .catch(function (error) {
+            console.error("Error adding document: ", error);
+          });
+      } else {
+        db.collection("user")
+          .doc(this.newUser.id)
+          .set(this.newUser)
+          .catch(function (error) {
+            console.error("Error writing document: ", error);
+          });
+      }
 
-     console.log("this.newUsepasooooooor--->>>>",this.newUser);
-    
-
-      /*
-      let horarioActual = new Date().getHours() + ":" + new Date().getMinutes();
-      let mensajeNuevo = {
-        horario: horarioActual,
-        contenido: this.contenidoEnviar,
-        asesor: true,
-        bot: false,
+      this.error = "";
+      this.title = "Add a New User";
+      this.newUser = {
+        id: "new",
+        name: "",
+        last: "",
+        email: "",
+        country: "",
       };
-      // this.conversas[this.indiceActivo].mensajes.push(mensajeNuevo);
-      let docRef = db
-        .collection("chats")
-        .doc(this.conversas[this.indiceActivo].id);
-
-      docRef.update({
-        mensajes: firebase.firestore.FieldValue.arrayUnion(mensajeNuevo),
-      });
-
-      /*
-        docRef.set({
-          mensajes:this.conversas[this.indiceActivo].mensajes},
-          { merge: true }
-        );
-        */
-     // this.contenidoEnviar = "";
-    },
-    asignaChat: function (indice, id) {
-      this.indiceActivo = indice;
-      let docRef = db.collection("chats").doc(id);
-      let email = this.email;
-      let asesor = this.usuario;
-
-      docRef
-        .get()
-        .then(function (doc) {
-          if (doc.data().correo_asesor.length == 0) {
-            docRef.update({
-              correo_asesor: email,
-              nombre_asesor: asesor,
-            });
-            /*
-            docRef.set(
-              {
-                correo_asesor: email,
-                nombre_asesor: asesor,
-              },
-              { merge: true }
-            );
-            */
-          }
-        })
-        .catch(function (error) {
-          console.log("Error en la asignacion-->>", error);
-        });
     },
     deleteUser: function (index) {
-      if(confirm('Are you sure delete it?')) {
-        console.log("eliminado-->", index);
+      if (confirm("Are you sure delete it?")) {
+        db.collection("user")
+          .doc(index)
+          .delete()
+          .catch(function (error) {
+            console.error("Error removing document: ", error);
+          });
       }
-    }
+    },
+    editUser: function (user) {
+      this.title = "Edit User: " + user.name;
+      this.newUser = user;
+    },
+    cancelEdit: function () {
+      if (confirm("Are you sure cancel Edit?")) {
+        this.title = "Add a New User";
+        this.newUser = {
+          id: "new",
+          name: "",
+          last: "",
+          email: "",
+          country: "",
+        };
+      }
+    },
   },
 };
 </script>
